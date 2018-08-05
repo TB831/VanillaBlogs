f77441b551beb43f054bcf97127f3b8fc08b3247
TB831 <druu831@gmail.com>
2018-08-05 11:34:02 -0700

    PostsIndex.js successfully makes request to our API

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index.js';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts }) (PostsIndex);  // Since key/value are named that same, we can use fetchPosts instead of fetchPosts: fetchPosts
