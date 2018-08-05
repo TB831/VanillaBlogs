5341a3eeb89363c7c4e1035ae06786dedf2d7dbd
TB831 <druu831@gmail.com>
2018-08-05 13:08:32 -0700

    PostsIndex component now render posts from state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=vanillablogskey' // API KEY requires a unique id
+
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=vanillablogskey'; // API KEY requires a unique id
 
 export function fetchPosts() {  // Action creator
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
   return {
     type: FETCH_POSTS,
     payload: request
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import PostsReducer from './reducersPosts.js';
 
 const rootReducer = combineReducers({
-  post: PostsReducer
+  posts: PostsReducer
 });
 
 export default rootReducer;
