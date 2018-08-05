0b776b53d6d2af46a91383bab288fd272ab87c6e
TB831 <druu831@gmail.com>
2018-08-05 13:44:48 -0700

    new component PostsNew. boilerplate setup

---
+++ src/components/PostsNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPosts extends Component {
+  render() {
+    return (
+      <div>
+        Create New Post!
+      </div>
+    );
+  }
+}
+
+export default NewPosts;
