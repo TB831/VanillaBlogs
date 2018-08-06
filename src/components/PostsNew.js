1c37bd49095abe91e3d8981a344d9cef13532328
TB831 <druu831@gmail.com>
2018-08-06 12:20:58 -0700

    wired up titleField for PostsNew

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-class NewPosts extends Component {
-  render() {
+class PostsNew extends Component {
+  renderTitleField(field) { // Field contains event handlers to the event were wiring up
     return (
       <div>
-        Create New Post!
+      <input
+        type='text'
+        {...field.input}
+      />
       </div>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field
+          name='title'
+          component={this.renderTitleField}
+        />
+      </form>
     );
   }
 }
 
-export default NewPosts;
+export default reduxForm({
+  form: 'PostsNewForm'  // form = name of the form
+})(PostsNew);
